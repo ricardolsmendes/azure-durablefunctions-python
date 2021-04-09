0ddc9ea7a0e5d1f77fa0f9ef4a775f07c3b5f765
Ricardo Mendes <ricardolsmendes@gmail.com>
2021-04-09 18:47:00 -0300

    Remove unnecessary code

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -1,15 +1,6 @@
 # This function is not intended to be invoked directly. Instead it will be
 # triggered by an HTTP starter function.
-# Before running this sample, please:
-# - create a Durable activity function (default name is "Hello")
-# - create a Durable HTTP starter function
-# - add azure-functions-durable to requirements.txt
-# - run pip install -r requirements.txt
 
-import logging
-import json
-
-import azure.functions as func
 import azure.durable_functions as df
 
 
@@ -18,5 +9,6 @@ def orchestrator_function(context: df.DurableOrchestrationContext):
     result2 = yield context.call_activity('Hello', "Seattle")
     result3 = yield context.call_activity('Hello', "London")
     return [result1, result2, result3]
+
 
 main = df.Orchestrator.create(orchestrator_function)
