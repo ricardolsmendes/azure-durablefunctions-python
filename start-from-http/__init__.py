488c1f2d83c56cc44584d11983c5e6eb92c6d508
Ricardo Mendes <ricardolsmendes@gmail.com>
2021-04-12 21:54:34 -0300

    Replace double with single quotes

--- start-from-http/__init__.py
+++ start-from-http/__init__.py
@@ -8,8 +8,8 @@ import azure.durable_functions as df
 
 async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
     client = df.DurableOrchestrationClient(starter)
-    instance_id = await client.start_new(req.route_params["functionName"], None, req.get_json())
+    instance_id = await client.start_new(req.route_params['functionName'], None, req.get_json())
 
-    logging.info(f"Started orchestration with ID = '{instance_id}'.")
+    logging.info(f'Started orchestration with ID = "{instance_id}".')
 
     return client.create_check_status_response(req, instance_id)
